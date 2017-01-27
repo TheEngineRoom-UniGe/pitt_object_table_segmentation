7ddcc50f916d38e74f2c70cc5aae2d3deb664457
alessio-cpt <alessio.capitanelli@gmail.com>
2017-01-27 16:25:55 +0100

    cleanup

--- catkin_generated/package.cmake
+++
@@ -1,7 +0,0 @@
-set(_CATKIN_CURRENT_PACKAGE "pitt_object_table_segmentation")
-set(pitt_object_table_segmentation_MAINTAINER "Buoncompagni Luca <luca.buoncompagni@edu.unige.it>")
-set(pitt_object_table_segmentation_DEPRECATED "")
-set(pitt_object_table_segmentation_VERSION "1.0.0")
-set(pitt_object_table_segmentation_BUILD_DEPENDS "message_generation" "pcl_conversions" "roscpp" "sensor_msgs" "std_msgs" "pcl_msgs" "pitt_msgs")
-set(pitt_object_table_segmentation_RUN_DEPENDS "message_runtime" "message_generation" "pcl_conversions" "roscpp" "sensor_msgs" "std_msgs" "pcl_msgs" "pitt_msgs")
-set(pitt_object_table_segmentation_BUILDTOOL_DEPENDS "catkin")
