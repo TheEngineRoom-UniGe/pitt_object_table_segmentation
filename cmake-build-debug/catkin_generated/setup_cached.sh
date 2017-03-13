d83d55d270ca2a3890ce458b29d949ef9bce54f9
carlotta sartore <carlotta.sartore1@gmail.com>
2017-03-13 17:54:49 +0100

    add

---
+++ cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/carlotta/catkin_ws/src/pitt_object_table_segmentation/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:."
+export ROSLISP_PACKAGE_DIRECTORIES="/home/carlotta/catkin_ws/src/pitt_object_table_segmentation/cmake-build-debug/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/carlotta/catkin_ws/src/pitt_object_table_segmentation:$ROS_PACKAGE_PATH"
